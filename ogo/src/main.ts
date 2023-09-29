ce41609f5296f8049ab6494ec9b8791c0e6f52ad
David Sang <dvdsang47@gmail.com>
2023-09-28 23:36:28 -0700

    fixed typescript error

--- ogo/src/main.ts
+++ ogo/src/main.ts
@@ -1,8 +1,14 @@
 import './app.css'
 import App from './App.svelte'
 
+const targetElement = document.getElementById('app');
+
+if (!targetElement) {
+  throw new Error("Couldn't find target element with ID 'app'");
+}
+
 const app = new App({
-  target: document.getElementById('app'),
+  target: targetElement,
 })
 
 export default app
