c93bcf18b527300b8602cc2464c9e63aefe4e3ec
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-10-09 17:32:21 -0700

    edit the scripts

---
+++ startup.bat
@@ -0,0 +1,8 @@
+echo Starting up, make sure Docker is installed!
+
+echo Building docker compose and running it
+
+docker-compose up --build 
+
+docker-compose down
+
---
+++ startup.sh
@@ -0,0 +1,7 @@
+echo Starting up, make sure Docker is installed!
+
+echo building docker compose and running it
+
+docker compose up --build 
+
+docker compose down
