3a797c5904999b164893835f803cee597b6a5dde
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-10-11 16:39:53 -0700

    remove start up scripts

--- startup.bat
+++
@@ -1,8 +0,0 @@
-echo Starting up, make sure Docker is installed!
-
-echo Building docker compose and running it
-
-docker-compose up --build 
-
-docker-compose down
-
--- startup.sh
+++
@@ -1,7 +0,0 @@
-echo Starting up, make sure Docker is installed!
-
-echo building docker compose and running it
-
-docker compose up --build 
-
-docker compose down
