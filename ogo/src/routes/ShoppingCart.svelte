f1d1c8e445bdfaea7e0927f77b77b18114800ff7
David Sang <dvdsang47@gmail.com>
2023-10-23 14:53:34 -0700

    changed layout

--- ogo/src/routes/ShoppingCart.svelte
+++ ogo/src/routes/ShoppingCart.svelte
@@ -5,39 +5,61 @@
 <html lang="en" data-theme="lemonade">
     <div class="flex flex-row">
         <div class="basis-1/2">
-            <h1>Order Summary</h1>
-            <div class="grid grid-rows-4 grid-flow-col gap-4">
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-                <div>04</div>
-                <div>05</div>
-                <div>06</div>
-                <div>07</div>
-                <div>08</div>
-                <div>09</div>
+            <div class="card bg-base-100 border-2 border-black-500 mt-8 mb-8">
+                <div class="card-body">
+                    <h1 class="card-title mb-8">ORDER SUMMARY</h1>
+                    <div class="grid grid-rows-4 grid-flow-col gap-4">
+                        <div class="card bg-neutral text-neutral-content">
+                            <div class="card-body items-center text-center">
+                                <h2 class="card-title">Product01</h2>
+                            </div>
+                        </div>
+                        <div class="card bg-neutral text-neutral-content">
+                            <div class="card-body items-center text-center">
+                                <h2 class="card-title">Product02</h2>
+                            </div>
+                        </div>
+                        <div class="card bg-neutral text-neutral-content">
+                            <div class="card-body items-center text-center">
+                                <h2 class="card-title">Product03</h2>
+                            </div>
+                        </div>
+                        <div class="card bg-neutral text-neutral-content">
+                            <div class="card-body items-center text-center">
+                                <h2 class="card-title">Product04</h2>
+                            </div>
+                        </div>
+                        <div class="card bg-neutral text-neutral-content">
+                            <div class="card-body items-center text-center">
+                                <h2 class="card-title">Product05</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="basis-1/2">
-            <div class="card bg-base-100 border-2 border-black-500 mt-8">
+            <div class="card bg-base-100 border-2 border-black-500 mt-8 mb-8">
                 <div class="card-body">
-                    <h1 class="card-title">ORDER PRICE</h1>
+                    <h1 class="card-title mb-8">ORDER PRICE</h1>
                     <div class="flex flex-row">
                         <div class="basis-1/2">
-                            <p>Item(s) Subtotal</p>
-                            <p>Shipping</p>
-                            <p>Tax</p>
-                            <p>Total</p>
+                            <p class="text-center">Item(s) Subtotal</p>
+                            <p class="text-center">Shipping Costs</p>
+                            <p class="text-center">Taxes</p>
+                            <p class="text-center">Total Cost</p>
                         </div>
                         <div class="basis-1/2">
-                            <p>$0.00</p>
-                            <p>$0.00</p>
-                            <p>$0.00</p>
-                            <p>$0.00</p>
+                            <p class="text-center">$0.00</p>
+                            <p class="text-center">$0.00</p>
+                            <p class="text-center">$0.00</p>
+                            <p class="text-center">$0.00</p>
                         </div>
                     </div>
+                    <progress class="progress progress-primary mt-16" value="40" max="100"></progress>
+                    <p class="text-center">You are qualified for FREE shipping.</p>
                     <div class="flex justify-center">
-                        <button class="btn btn-secondary">Proceed to Payment</button>
+                        <button class="btn btn-secondary mt-16">Proceed to Payment</button>
                     </div>
                 </div>
             </div>
