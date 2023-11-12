f460ecccfc19a37a329072a75a9c61b75fdfd33f
townsag <townsag@gmail.com>
2023-11-12 00:18:04 -0800

    changed the way that the map component loads the google maps api library. Shifted responsibility for making the map reactive from the Map svelte component to the parent component of the map in order to deal with concurrencey and api loader issues. Exported functions from the map component to the parent component whcih allow the parent component to initialize the map as well as change the polyline or markers

--- ogo/src/lib/components/Map.svelte
+++ ogo/src/lib/components/Map.svelte
@@ -1,28 +1,53 @@
 <script lang="ts">
     import { Loader } from "@googlemaps/js-api-loader"
-
+  import { onMount } from "svelte";
+    
     export let API_KEY: string;
-    export let map_data: any;
 
     let map: google.maps.Map;
     let container: HTMLElement;
-    
+    let polyline: google.maps.Polyline | null = null;
+    let markers: google.maps.Marker[] = [];
+    let Map: typeof google.maps.Map
+    // let googleMaps: typeof google.maps;
+
+    // let GeometryLibrary: google.maps.GeometryLibrary;
+    // let Polyline: typeof google.maps.Polyline;
+
     const loader = new Loader({
         apiKey: API_KEY,
         version: "weekly",
         libraries: ["geometry"]
     });
 
-    loader.load().then(async () => {
-        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-        map = new Map(container, {
-            center: { lat: 37.7749, lng: -122.4194 },
-            zoom: 8,
-        });
+    export async function init_map(){
+        console.log("entering init map");
+        await loader.load();
+        const MapsLibrary = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+        Map = MapsLibrary.Map;
 
-        var encodedPolyline = map_data["polyline"];
+        // GeometryLibrary = await google.maps.importLibrary("geometry") as google.maps.GeometryLibrary;
+        // Polyline = MapsLibrary.Polyline;
+        // console.log("finished loading polyline library");
+
+
+        map = new Map(container, {
+            center: { lat: 37.330120, lng: -121.877430 },
+            zoom: 10,
+        });
+        console.log("leaving init map");
+    }
+
+    export async function add_polyline(encodedPolyline: string){
+        console.log("inside add polyline");
+        await loader.load();
+        remove_polyline();
+        // console.log("this is the polyline: ", encodedPolyline);
+        console.log(google.maps);
+        // var decodedPath = google.maps.geometry.encoding.decodePath(encodedPolyline);
         var decodedPath = google.maps.geometry.encoding.decodePath(encodedPolyline);
-        var polyline = new google.maps.Polyline({
+        // console.log("this is the decoded path: ", decodedPath);
+        polyline = new google.maps.Polyline({
             path: decodedPath,
             geodesic: true,
             strokeColor: '#0000FF',
@@ -30,20 +55,79 @@
             strokeWeight: 3
         });
         polyline.setMap(map);
+        console.log("this is map: ", map);
+        console.log("after set map in add polyline");
+    }
 
-        for(const leg of map_data.legs){
-            new google.maps.Marker({
-                position: {lat: leg.lat, lng: leg.lon},
-                map,
-                label: String(leg.sequence)
-            });
+    export function remove_polyline(){
+        if (polyline) {
+            polyline.setMap(null);
         }
+    }
 
-    //     new google.maps.Marker({ position: { lat: 37.3365145, lng: -121.88460509999999 }, map, label: "0" });
-    //     new google.maps.Marker({ position: { lat: 37.3348697, lng: -121.89332780000001 }, map, label: "1" });
-    //     new google.maps.Marker({ position: { lat: 37.3416232, lng: -121.89058250000001 }, map, label: "2" });
-    //     new google.maps.Marker({ position: { lat: 38.074518999999995, lng: -122.706558 }, map, label: "3" });
-    });
+    export function add_markers(marker_data: {[key: string]: any}[]){
+        console.log("adding markers");
+        remove_markers();
+        marker_data.forEach(marker => {
+            markers.push(new google.maps.Marker({
+                position: {lat: marker.lat, lng: marker.lon}, 
+                map, 
+                label:String(marker.sequence)}));
+        });
+    }
+
+    export function remove_markers(){
+        // first hide all active markers
+        for (let i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        markers = [];
+    }
+
+    // onMount(async ()=>{
+    //     await init_map();}
+    //     );
+
+    // import { Loader } from "@googlemaps/js-api-loader"
+
+    // export let API_KEY: string;
+    // export let map_data: any;
+
+    // let map: google.maps.Map;
+    // let container: HTMLElement;
+    
+    // const loader = new Loader({
+    //     apiKey: API_KEY,
+    //     version: "weekly",
+    //     libraries: ["geometry"]
+    // });
+
+    // loader.load().then(async () => {
+    //     const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+    //     map = new Map(container, {
+    //         center: { lat: 37.7749, lng: -122.4194 },
+    //         zoom: 9,
+    //     });
+
+    //     var encodedPolyline = map_data["polyline"];
+    //     var decodedPath = google.maps.geometry.encoding.decodePath(encodedPolyline);
+    //     var polyline = new google.maps.Polyline({
+    //         path: decodedPath,
+    //         geodesic: true,
+    //         strokeColor: '#0000FF',
+    //         strokeOpacity: 1.0,
+    //         strokeWeight: 3
+    //     });
+    //     polyline.setMap(map);
+
+    //     for(const leg of map_data.legs){
+    //         new google.maps.Marker({
+    //             position: {lat: leg.lat, lng: leg.lon},
+    //             map,
+    //             label: String(leg.sequence)
+    //         });
+    //     }
+    // });
 
 </script>
 
