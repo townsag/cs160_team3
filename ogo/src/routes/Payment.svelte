99d3fc693dc3f9ad6d0ef401b02f9fe8aedf1197
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-11-13 15:23:39 -0800

    decimals

--- ogo/src/routes/Payment.svelte
+++ ogo/src/routes/Payment.svelte
@@ -170,10 +170,10 @@
             <!-- New card content goes here -->
             <div class="card-body pl-20 pr-20 pt-10">
                 <h1 class="card-title">Order Price</h1>
-                <h1 class="card-title">Cart:<p style="text-align: right;">{price}</p> </h1>
-                <h1 class="card-title">Tax:<p style="text-align: right;">{price * 0.05}</p> </h1>
-                <h1 class="card-title">Shipping:<p style="text-align: right;">{price * 0.1}</p> </h1>
-                <h1 class="card-title">Total:<p style="text-align: right;">{(price * 0.05) + (price * 0.1) + price}</p> </h1>
+                <h1 class="card-title">Cart:<p style="text-align: right;">{(price).toFixed(2)}</p> </h1>
+                <h1 class="card-title">Tax:<p style="text-align: right;">{(price * 0.05).toFixed(2)}</p> </h1>
+                <h1 class="card-title">Shipping:<p style="text-align: right;">{(price * 0.1).toFixed(2)}</p> </h1>
+                <h1 class="card-title">Total:<p style="text-align: right;">{((price * 0.05) + (price * 0.1) + price).toFixed(2)}</p> </h1>
                 <button on:click={handlePaymentSubmit} class="btn bg-secondary mt-6">Pay Now</button>
             </div>
         </div>
