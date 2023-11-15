13798cc06caa613fd8aa0745bfbff9815539cbef
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-11-14 19:40:00 -0800

    splash page

--- ogo/src/App.svelte
+++ ogo/src/App.svelte
@@ -6,7 +6,7 @@
   import UserSettings from "./routes/UserSettings.svelte";
   import EmployeeProduct from "./routes/EmployeeProduct.svelte";
   import ShoppingCart from "./routes/ShoppingCart.svelte";
-  //import Navbar from "./lib/components/Navbar.svelte";
+  import Navbar from "./lib/components/Navbar.svelte";
   import Footer from "./lib/components/Footer.svelte";
   import EmMaps from "./routes/EmployeeMaps.svelte";
   import Browse from "./routes/Browse.svelte";
@@ -30,14 +30,52 @@
     { path: "/payment", component: Payment},
     { path: "/employee-user-management", component: EmployeeUserManagement},
   ];
-
   export let url = "";
+  let currentUrl = window.location.pathname;
 </script>
 
 <html lang="en" data-theme="lemonade">
   <main>
     <Router {url}>
-      
+      {#if currentUrl == "/"}
+        <Navbar/>
+          <div class="flex justify-center mt-6"> 
+            <h1 class="text-6xl mt-4"> Welcome to OGO!</h1>
+          </div>
+          <div class="flex justify-center mt-6"> 
+            <nav>
+              <div class="mb- grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-center justify-between p-28">
+                <a
+                  href="/login"
+                  class="group rounded-lg border border-transparent px-5 py-4 hover:bg-[#6a994e] transition-colors duration-300 ease-in-out  "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h2 class={`mb-3 text-2xl font-neutral`}>
+                    Login{' '}
+                  </h2>
+                  <p class={`m-0 max-w-[100ch] text-sm opacity-50 sm:text-center text-center`}>
+                    Login to access the forum page and chat with others!
+                  </p>
+                </a>
+        
+                <a
+                  href="/signup"
+                  class="group rounded-lg border border-transparent px-5 py-4 hover:bg-[#6a994e] hovor:color-white transition-colors duration-300 ease-in-out  "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h2 class={`mb-3 text-2xl font-neutral`}>
+                    Signup{' '}
+                  </h2>
+                  <p class={`m-0 max-w-[100ch] text-sm opacity-50 sm:text-center text-center`}>
+                    Sign up to start experiencing the future!
+                  </p>
+                </a>
+              </div>
+            </nav>
+          </div>
+        {/if}
       <div>
         {#each routes as route}
           <Route path={route.path} component={route.component} />
