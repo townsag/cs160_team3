c2c867560eef054e13f1954af86e147a49ea1d94
townsag <townsag@gmail.com>
2023-11-15 15:19:43 -0800

    pulled wills changes to the customer order history page and its components. Lots of great styling

--- ogo/src/lib/components/OrderSummaryCustomer.svelte
+++ ogo/src/lib/components/OrderSummaryCustomer.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
+
+    import OrderTable from "./OrderTable.svelte";
 
     export let order_id: number;
     export let status: number; //0: placed, 1: in progress, 2: delivered
@@ -8,29 +10,20 @@
     export let total_price: number;
     export let total_weight: number;
 
-    let extend_flag: Boolean = false;
-    let button_text: string = "see more";
-    function onClick() {
-        extend_flag = !extend_flag
-        if (extend_flag){
-            button_text = "see less";
-        } else {
-            button_text = "see more";
-        }
-    }
-
     function epochToDate(epoch: number) {
         return new Date(epoch * 1000); // Multiply by 1000 to convert seconds to milliseconds
     }
 
     let order_date: Date = epochToDate(placed_epoch);
     let eta_date: Date = epochToDate(eta_epoch);
-    let order_product_info: any = []
+    let order_product_info: any[] = [];
 
-    async function get_order_products(){
+    async function get_order_products() {
         try {
-            const order_products_response = await fetch(`/getOrderItems?orderID=${order_id}`);
-            if (order_products_response.ok){
+            const order_products_response = await fetch(
+                `/getOrderItems?orderID=${order_id}`
+            );
+            if (order_products_response.ok) {
                 order_product_info = await order_products_response.json();
                 console.log("order products response: ", order_product_info);
             } else {
@@ -41,43 +34,36 @@
             console.log("error: ", error);
         }
     }
-    onMount(async () =>{
+    onMount(async () => {
         await get_order_products();
-    })
-
-
+    });
 </script>
 
-<div class="bg-green-500 h-1/6 w-full rounded-xl rounded-xl border-black border-2">
-    <div class="flex flex-row place-content-around">
-        <div class="text-md text-yellow-200 p-2">Order num: {order_id}</div>
-        <div class="text-md text-yellow-200 p-2">Placed on: {order_date.getMonth()}/{order_date.getDay()}/{order_date.getFullYear()}</div>
-        <button class="bg-yellow-200 rounded-lg w-1/6 p-1 m-2" on:click={onClick}>{button_text}</button>
-    </div>
-    <div class="flex justify-center p-2">
-        <!-- <progress class="progress progress-secondary w-56" value={value} max="100"></progress> -->
-        <progress class="progress w-5/6" value={100*(status+1)/3} max="100"></progress>
-    </div>
-    {#if extend_flag}
-        <div class="flex flex-row justify-evenly">
-            <div class="text-md text-yellow-200 p-2">order price: {total_price}</div>
-            <div class="text-md text-yellow-200 p-2">order weight: {total_weight}</div>
-            {#if eta_epoch != null}
-                <div class="text-md text-yellow-200 p-2">ETA: {eta_date.getMonth()}/{eta_date.getDay()}/{eta_date.getFullYear()}</div>
-            {:else}
-                <div class="text-md text-yellow-200 p-2">ETA: pending</div>
-            {/if}
-        </div>
-        {#if (order_product_info.length > 0)}
-            <div class="flex flex-col justify-evenly p-2">
-                {#each order_product_info as product}
-                    <div class="flex flex-row justify-evenly bg-yellow-200 rounded-xl m-2">
-                        <div class="text-md p-2">Product Name: {product.name}</div>
-                        <div class="text-md p-2">Product quantity: {product.quantity}</div>
-                    </div>
-                {/each}
+<details class="collapse collapse-arrow border">
+    <summary class="collapse-title">
+        <div class="flex flex-row place-content-around">
+            <div class="text-xl font-medium">Order #{order_id}</div>
+            <div>
+                {order_date.getMonth()}/{order_date.getDay()}/{order_date.getFullYear()}
             </div>
+            <div>${total_price.toFixed(2)}</div>
+        </div>
+        <div class="flex flex-row place-content-around mt-2">
+            <progress
+                class="progress w-5/6"
+                value={(100 * (status + 1)) / 3}
+                max="100"
+            />
+        </div>
+    </summary>
+    <div class="collapse-content">
+        {#if eta_epoch != null}
+            <div class="p-2">
+                ETA: {eta_date.getMonth()}/{eta_date.getDay()}/{eta_date.getFullYear()}
+            </div>
+        {:else}
+            <div class="p-2">ETA: pending</div>
         {/if}
-    {/if}
-
-</div>
+        <OrderTable {order_id} />
+    </div>
+</details>
